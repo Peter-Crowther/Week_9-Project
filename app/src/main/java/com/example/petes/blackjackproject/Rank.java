c87c7f04a6037b1bb41235e776708bcd6b42d7a6
Peter Crowther <petercrowther90@hotmail.com>
2017-11-18 11:46:53 +0000

    created rank file

---
+++ app/src/main/java/com/example/petes/blackjackproject/Rank.java
@@ -0,0 +1,28 @@
+package com.example.petes.blackjackproject;
+
+/**
+ * Created by petes on 18/11/2017.
+ */
+
+public enum Rank {
+    KING(13),
+    QUEEN(12),
+    JACK(11),
+    TEN(10),
+    NINE(9),
+    EIGHT(8),
+    SEVEN(7),
+    SIX(6),
+    FIVE(5),
+    FOUR(4),
+    THREE(3),
+    TWO(2),
+    ACE(1);
+
+    private int value;
+
+    Rank(int value){
+        this.value = value;
+    }
+
+}
