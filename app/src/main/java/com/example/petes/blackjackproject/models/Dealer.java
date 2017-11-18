f584f88ddb320110b2c0ed5c422b3c57df27a0c2
Peter Crowther <petercrowther90@hotmail.com>
2017-11-18 14:17:16 +0000

    created dealer class

---
+++ app/src/main/java/com/example/petes/blackjackproject/models/Dealer.java
@@ -0,0 +1,11 @@
+package com.example.petes.blackjackproject.models;
+
+/**
+ * Created by petes on 18/11/2017.
+ */
+
+public class Dealer extends Player {
+    public Dealer(String name) {
+        super(name);
+    }
+}
