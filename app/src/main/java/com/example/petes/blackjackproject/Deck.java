73a4abf5cae66b4bf280205c4a32cb2fd66b2634
Peter Crowther <petercrowther90@hotmail.com>
2017-11-18 13:35:08 +0000

    created deck class

---
+++ app/src/main/java/com/example/petes/blackjackproject/Deck.java
@@ -0,0 +1,16 @@
+package com.example.petes.blackjackproject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by petes on 18/11/2017.
+ */
+
+public class Deck {
+
+    ArrayList<Card> cards;
+
+
+
+
+}
