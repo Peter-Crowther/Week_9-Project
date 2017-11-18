7ead33137dfbc4fa19f2d78bf9a527c0fdfd9694
Peter Crowther <petercrowther90@hotmail.com>
2017-11-18 14:18:11 +0000

    created game class

---
+++ app/src/main/java/com/example/petes/blackjackproject/models/Game.java
@@ -0,0 +1,8 @@
+package com.example.petes.blackjackproject.models;
+
+/**
+ * Created by petes on 18/11/2017.
+ */
+
+public class Game {
+}
---
+++ app/src/test/java/com/example/petes/blackjackproject/DealerTest.java
@@ -0,0 +1,8 @@
+package com.example.petes.blackjackproject;
+
+/**
+ * Created by petes on 18/11/2017.
+ */
+
+public class DealerTest {
+}
