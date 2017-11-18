95d60f4baf8196798055eba5234510e83aee07e4
Peter Crowther <petercrowther90@hotmail.com>
2017-11-18 11:59:55 +0000

    created card class

---
+++ app/src/main/java/com/example/petes/blackjackproject/Card.java
@@ -0,0 +1,20 @@
+package com.example.petes.blackjackproject;
+
+/**
+ * Created by petes on 18/11/2017.
+ */
+
+public class Card {
+
+    Suit suit;
+    Rank rank;
+
+    public Card() {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+
+
+
+}
